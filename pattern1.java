3b7aa8a499a095b2c8f93c04481009a03fb6a786
mahesh1632 <mahesh.lahane@gmail.com>
2017-05-23 09:56:27 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,27 @@
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class pattern1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i =0; i<5;i++)
+		{
+		    for (int j=0; j<(5-i); j++)
+		    {
+		        System.out.print("* ");
+		    }
+		   System.out.println();
+		 
+		}
+	}
+}
