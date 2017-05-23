06885117667605dd55fdc7c0b86848614706b879
mahesh1632 <mahesh.lahane@gmail.com>
2017-05-23 16:47:17 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,36 @@
+/*
+
+
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * *
+
+*/
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Pattern2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=0; i <=5 ;i++)
+		{
+		    for (int j=0; j <=5; j++)
+		    {
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		 
+		  
+		 
+		}
+	}
+}
